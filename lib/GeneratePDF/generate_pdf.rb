78f5322f9c37eff5d495b16b957bce798ef778a2
ramanbedi1989 <ramanbedi1989@gmail.com>
2013-11-17 20:46:54 +0530

    modify pdf generator

--- lib/GeneratePDF/generate_pdf.rb
+++ lib/GeneratePDF/generate_pdf.rb
@@ -10,17 +10,18 @@ class GeneratePDF
           bounding_box = pdf.bounds
           b_height = bounding_box.height
           b_width = bounding_box.width
-          initial_line = 173
-          initial_width = 70
-          line_diff = 25
+          initial_line = 177
+          initial_width = 87
+          line_diff = 27
           pdf.translate(b_width/2,b_height/2) do
             pdf.rotate 90 do
               pdf.draw_text "#{patient.name}", at: [initial_width,initial_line] 
-              pdf.draw_text "#{patient.age}", at: [initial_width + 152,initial_line]
-              pdf.draw_text "#{patient.gender}", at: [initial_width + 207,initial_line]
-              pdf.draw_text "#{patient.created_at.to_date.strftime('%d-%m-%Y')}", at: [initial_width + 257,initial_line]
-              pdf.draw_text "#{patient.father_spouse}", at: [initial_width + 32,initial_line-line_diff]
-              pdf.draw_text "#{patient.address}", at: [initial_width + 171 , initial_line-line_diff]
+              pdf.draw_text "#{patient.age}", at: [initial_width + 155,initial_line]
+              pdf.draw_text "#{patient.gender}", at: [initial_width + 210,initial_line]
+              pdf.draw_text "#{patient.created_at.to_date.strftime('%d-%m-%Y')}", at: [initial_width + 265,initial_line]
+              pdf.draw_text "#{patient.father_spouse}", at: [initial_width + 35,initial_line-line_diff]
+              pdf.draw_text "#{patient.address}", at: [initial_width + 174 , initial_line-line_diff]
+              pdf.draw_text "#{patient.id}", at: [initial_width + 35 , initial_line-(2*line_diff)]
               lower_line_diff = 15
               index = 0
               patient.medical_departments.each do |dept|
