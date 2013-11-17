e483c953031fc9ebccddeb98ee196e67d7385deb
ramanbedi1989 <ramanbedi1989@gmail.com>
2013-11-17 19:50:14 +0530

    make new columns mass assignable

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-  attr_accessible :address, :age, :date, :gender, :name, :id_proof_number, :mobile,:father_spouse, :town_id, :id_proof_id, :medical_department_ids
+  attr_accessible :address, :age, :date, :gender, :name, :id_proof_number, :mobile,:father_spouse, :town_id, :id_proof_id, :medical_department_ids, :treatment, :medicines, :services, :special_instructions
   VALID_GENDERS = ["Male" , "Female"]
   belongs_to :id_proof 
   belongs_to :town
